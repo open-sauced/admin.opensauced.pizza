24fe36fc031fa26c8ddfddcfa2ef7e8c3f95b874
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2021-02-05 17:39:18 -0500

    fix broken input

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -5,7 +5,7 @@ const HomePage = () => {
     <>
       <h1>HomePage</h1>
       <form>
-        <input style="leftPa" className="focus:border-light-blue-500 focus:ring-1 focus:ring-light-blue-500 focus:outline-none w-full text-sm text-black placeholder-gray-500 border border-gray-200 rounded-md py-2 pl-10" type="text" aria-label="Repository Name" placeholder="open-sauced/flava-flav" />
+        <input className="focus:border-light-blue-500 focus:ring-1 focus:ring-light-blue-500 focus:outline-none w-full text-sm text-black placeholder-gray-500 border border-gray-200 rounded-md py-2 pl-10" type="text" aria-label="Repository Name" placeholder="open-sauced/flava-flav" />
       </form>
       <p>
         My default route is named <code>home</code>, link to me with `
