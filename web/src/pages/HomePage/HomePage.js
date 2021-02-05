c335fd62af945a8c482e8480f5a287f94e9f688f
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2021-02-05 16:14:53 -0500

    adds home page and tailwind

---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,19 @@
+import { Link, routes } from '@redwoodjs/router'
+
+const HomePage = () => {
+  return (
+    <>
+      <h1>HomePage</h1>
+      <p>
+      <input class="focus:border-light-blue-500 focus:ring-1 focus:ring-light-blue-500 focus:outline-none w-full text-sm text-black placeholder-gray-500 border border-gray-200 rounded-md py-2 pl-10" type="text" aria-label="Repository Name" placeholder="open-sauced/flava-flav" />
+
+      </p>
+      <p>
+        My default route is named <code>home</code>, link to me with `
+        <Link to={routes.home()}>Home</Link>`
+      </p>
+    </>
+  )
+}
+
+export default HomePage
