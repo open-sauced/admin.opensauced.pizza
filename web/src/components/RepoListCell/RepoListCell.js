81ebf5c7cccd905293cea897b15f90f7cd0a7a13
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2021-02-23 19:22:59 -0500

    ui on page

--- web/src/components/RepoListCell/RepoListCell.js
+++ web/src/components/RepoListCell/RepoListCell.js
@@ -2,6 +2,7 @@ export const QUERY = gql`
   query RepoListQuery {
     repositories {
       id
+      fullName
     }
   }
 `
@@ -13,5 +14,13 @@ export const Empty = () => <div>Empty</div>
 export const Failure = ({ error }) => <div>Error: {error.message}</div>
 
 export const Success = ({ repositories }) => {
-  return JSON.stringify(repositories)
+  return repositories.map((repository) => (
+    <article key={repository.id}>
+      <header>
+        <div class="text-gray-600 font-semibold text-lg mb-2 mx-3 px-2">
+          {repository.fullName}
+        </div>{' '}
+      </header>
+    </article>
+  ))
 }
--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,13 +1,18 @@
-import { Link, routes } from '@redwoodjs/router'
-import RepoListCell from 'src/components/RepoListCell'
+import { Link, routes } from "@redwoodjs/router";
+import RepoListCell from "src/components/RepoListCell";
 
 const HomePage = () => {
   return (
     <>
       <h1>List of Repos in the DB</h1>
-      <RepoListCell />
-    </>
-  )
-}
 
-export default HomePage
+      <div class="">
+        <div class="flex max-w-xl my-10 bg-white shadow-md rounded-lg overflow-hidden mx-auto">
+          <RepoListCell />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
