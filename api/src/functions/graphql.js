916d3af0f958466b5e4905104c07b599f804cf4b
Anthony Campolo <anthony@stepzen.com>
2021-09-01 18:58:31 -0700

    add logger

--- api/src/functions/graphql.js
+++ api/src/functions/graphql.js
@@ -6,16 +6,20 @@ import {
 
 import schemas from 'src/graphql/**/*.{js,ts}'
 import { db } from 'src/lib/db'
+import { logger } from 'src/lib/logger'
+
 import services from 'src/services/**/*.{js,ts}'
 
 import { getCurrentUser } from 'src/lib/auth'
 
 export const handler = createGraphQLHandler({
+  loggerConfig: { logger, options: {} },
   getCurrentUser,
   schema: makeMergedSchema({
     schemas,
     services: makeServices({ services }),
   }),
+
   onException: () => {
     // Disconnect from your database with an unhandled exception.
     db.$disconnect()
